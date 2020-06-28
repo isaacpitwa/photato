2f1ea23ef84071358e4221eff39536352791b6a3
Isaac Pitwa <isaacpitwa256@gmail.com>
2020-06-29 00:49:14 +0300

    fetch API photos

--- web/web/src/app.container.js
+++ web/web/src/app.container.js
@@ -1,26 +1,10 @@
 import React ,{ useState,useEffect }from 'react';
 import PhotoGallery from 'react-photo-gallery';
-import { uploadPhoto } from './api';
+import { uploadPhoto ,getPhotos} from './api';
 import Header from './header.component';
 
 
 function AppContainer() {
-    const photos = [{
-            src: 'http://placekitten.com/200/300',
-            width: 3,
-            height: 4,
-        },
-        {
-            src: 'http://placekitten.com/200/200',
-            width: 1,
-            height: 1,
-        },
-        {
-            src: 'http://placekitten.com/300/400',
-            width: 3,
-            height: 4,
-        },
-    ];
 
     const [isUploading, setIsUploading] = useState(false);
     const [photos, setPhotos] = useState([]);
